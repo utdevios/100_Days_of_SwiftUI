700b7aba1df75b0364765e11ec497724f82a5a1e
utdevios <utdevios@gmail.com>
2023-02-24 01:17:13 +0300

    Create README.md

---
+++ Deneme/Deneme/DenemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DenemeApp.swift
+//  Deneme
+//
+//  Created by Umit TERZIOGLU on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct DenemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
